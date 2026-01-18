d739e9f2256880ffae0206c4be3775566215d228
Coretex <valerash2@gmail.com>
2026-01-18 20:38:46 +0200

    cleanup

--- old/src/main/java/domain/JsonIO.java
+++
@@ -1,9 +0,0 @@
-package lv.lu.eztf.dn.combopt.evrp.domain;
-
-import ai.timefold.solver.jackson.impl.domain.solution.JacksonSolutionFileIO;
-
-public class JsonIO extends JacksonSolutionFileIO<EVRPsolution> {
-    public JsonIO() {
-        super(EVRPsolution.class);
-    }
-}
--- old/src/main/java/rest/ErrorInfo.java
+++
@@ -1,4 +0,0 @@
-package lv.lu.eztf.dn.combopt.evrp.rest;
-
-public record ErrorInfo(String jobId, String message) {
-}
